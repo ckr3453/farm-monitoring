a3fdf0356562daeb870d2ae83eb1dd0e5fb88407
ckr3453 <ckr3453@gmail.com>
2022-05-27 00:04:11 +0900

    [FIX] unused-files delete

--- src/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Card, CardContent, CardHeader } from '@mui/material';
-
-export default () => (
-    <Card>
-        <CardHeader title="FarmData Dashboard" />
-        <CardContent>Contents</CardContent>
-    </Card>
-);

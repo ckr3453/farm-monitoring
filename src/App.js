5d2220a75d7cb2ac0d9ad1ff95d5630431330c5c
ckr3453 <ckr3453@gmail.com>
2022-05-27 20:28:41 +0900

    도메인수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import simpleRestProvider from 'ra-data-simple-rest';
 import ContentPasteSearchTwoToneIcon from '@mui/icons-material/ContentPasteSearchTwoTone';
 
 const dataProvider = simpleRestProvider('http://geonchang.synology.me:8000')
-// const dataProvider = simpleRestProvider('http://localhost:8000')
+// const dataProvider = simpleRestProvider('http://175.195.139.88:8000')
 
 const App = () => (
     <Admin dataProvider={dataProvider}>
